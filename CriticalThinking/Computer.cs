1bbf1ae3913c443b89e0b2971cca143207a30755
Randall Gray <randall.gray7260@gmail.com>
2020-04-24 10:03:11 -0500

    Fun with Classes #2

---
+++ CriticalThinking/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{
+    class Computer
+    {
+        // Member variables
+
+        // constructor
+
+        // Member methods
+    }
+}
