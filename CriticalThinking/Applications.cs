a658a9ea296a12481670230bca07be4355ec1b4e
Randall Gray <randall.gray7260@gmail.com>
2020-04-24 10:35:09 -0500

    Fun with Classes #7

---
+++ CriticalThinking/Applications.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{
+    class Applications
+    {
+        // Member variables
+        public string applicationName;
+        public string applicationType;
+        public double requiredRAM;
+        public double requiredStorage;
+
+        // constructor
+        public Applications()
+        {
+            applicationName = "Slack";
+            applicationType = "Messaging service";
+            requiredRAM = 4.0;  //In Gigabytes
+            requiredStorage = 0.512;  //In Gigabytes
+        }
+
+        // Member methods
+    }
+}
