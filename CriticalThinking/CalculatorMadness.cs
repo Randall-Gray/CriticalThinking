5edf090d7f862fc7f419ffc529a04816db48f212
Randall Gray <randall.gray7260@gmail.com>
2020-04-24 09:27:09 -0500

    Fun with Methods #3

--- CriticalThinking/CalculatorMadness.cs
+++ CriticalThinking/CalculatorMadness.cs
@@ -13,14 +13,8 @@ namespace CriticalThinking
         // constructor
 
         // Member methods
-        public void AddTwoNumbers()
+        public void AddTwoNumbers(int numberOne, int numberTwo)
         {
-            Console.WriteLine("Please enter the first number: ");
-            int numberOne = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter the second number: ");
-            int numberTwo = int.Parse(Console.ReadLine());
-
             int result = numberOne + numberTwo;
             Console.WriteLine("The result of this addtion is: " + result);
         }
